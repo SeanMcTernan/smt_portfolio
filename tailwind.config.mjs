031d51cc9ccafef6ebaf49ce57816f1b539e1197
Sean Mc Ternan <seanmcternan@gmail.com>
2024-08-22 15:45:28 -0400

    Correct Font Path

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,7 +17,7 @@ module.exports = {
 				sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
 				serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
 				heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-				bold: ['YourBoldFont', ...defaultTheme.fontFamily.sans],
+				thin: ['Roboto-Thin', ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
